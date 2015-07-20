a08adc9708ed89d1f1864046f4a3eb9fe92c8119
dailianyu <89481109@qq.com>
2015-07-20 23:10:28 +0800

    Create frameworks_base_Android.mk

---
+++ android_from_kernel_to_app/frameworks_base_Android.mk
@@ -0,0 +1,4 @@
+LOCAL_SRC_FILES += /
+    core/java/android/os/IVibratorService.aidl /
+    core/java/android/os/IHelloService.aidl /
+    core/java/android/service/urlrenderer/IUrlRendererService.aidl /
